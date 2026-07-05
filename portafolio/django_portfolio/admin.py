7d71bd0e903234ab34055043c8ff872622707760
luisma <mrluisgamerps3@gmail.com>
2026-07-05 16:53:29 -0400

    pre railway

--- portafolio/django_portfolio/admin.py
+++ portafolio/django_portfolio/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Project, Social
+from .models import Project, Social, Technology
 
-admin.site.register(Project)
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    filter_horizontal = ("technologies",)
+
+
 admin.site.register(Social)
